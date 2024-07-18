c70c17b082161c890d919c65fb3f4a5a951cd534
Reactongraph <js.developer@ongraph.com>
2024-07-18 18:09:30 +0530

    card-page added

--- src/app/api/list/route.js
+++ src/app/api/list/route.js
@@ -2,18 +2,25 @@ import { google } from "googleapis";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const { 
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: CLIENT_ID, 
-    NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: CLIENT_SECRET, 
-    NEXT_PUBLIC_GOOGLE_REDIRECT_URI: REDIRECT_URI, 
-    NEXT_PUBLIC_GOOGLE_REFRESH_TOKEN: REFRESH_TOKEN 
+  const {
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: CLIENT_ID,
+    NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: CLIENT_SECRET,
+    NEXT_PUBLIC_GOOGLE_REDIRECT_URI: REDIRECT_URI,
+    NEXT_PUBLIC_GOOGLE_REFRESH_TOKEN: REFRESH_TOKEN,
   } = process.env;
 
   if (!CLIENT_ID || !CLIENT_SECRET || !REDIRECT_URI || !REFRESH_TOKEN) {
-    return NextResponse.json({ error: "Missing Google API credentials" }, { status: 400 });
+    return NextResponse.json(
+      { error: "Missing Google API credentials" },
+      { status: 400 }
+    );
   }
 
-  const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+  const oauth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI
+  );
   oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
   const drive = google.drive({ version: "v3", auth: oauth2Client });
@@ -21,7 +28,8 @@ export async function GET() {
   try {
     const response = await drive.files.list({
       pageSize: 15,
-      fields: "files(id, name)",
+      fields:
+        "files(id, name, webViewLink, webContentLink, mimeType, parents, size, createdTime, modifiedTime, owners, shared, permissions, thumbnailLink, description, viewedByMeTime, trashed)",
     });
     return NextResponse.json({ files: response.data.files });
   } catch (error) {
